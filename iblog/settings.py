6f926baf570208217729b3a8e5ee4392ce53a4a8
Victor Charles Kalungi <kalxcharles@gmail.com>
2020-07-12 15:04:09 +0300

    Update1

--- iblog/settings.py
+++ iblog/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = 'tlc8th0#%n(yu_re5*stjc4i428%mql8n7^*k+k2$v@b-ih3um'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
